518e1ed8e785c847610c863aa8adda158e51bc98
SpaceScape <71204304+Space-Scape@users.noreply.github.com>
2024-10-29 03:41:37 -0500

    Update memberwizard.py

--- memberwizard.py
+++ memberwizard.py
@@ -54,7 +54,7 @@ async def on_thread_create(thread):
     elif thread.parent.name == "become-a-member" and thread.name.startswith("Welcome-"):
         embed = discord.Embed(
             title="Welcome :wave:",
-            description="**Please upload screenshots of our base requirements and a staff member will help you when available.** :hourglass: \n\n"
+            description="**Please upload screenshots of our base requirements and a staff member will help you when available.** :hourglass: \n"
                         "## **Important:** :loudspeaker: ##\n"
                         "### 1. No Bank Screenshots! :no_entry_sign: :bank: ###\n"
                         "### 2. Full client screenshots with chatbox open :camera: ###\n"
