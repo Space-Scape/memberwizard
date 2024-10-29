5cebdddf079353224f81c4128d2e702fc87ed692
SpaceScape <71204304+Space-Scape@users.noreply.github.com>
2024-10-29 03:44:35 -0500

    Update memberwizard.py

--- memberwizard.py
+++ memberwizard.py
@@ -54,7 +54,7 @@ async def on_thread_create(thread):
     elif thread.parent.name == "become-a-member" and thread.name.startswith("Welcome-"):
         embed = discord.Embed(
             title="Welcome :wave:",
-            description="**Please upload screenshots of our base requirements and a staff member will help you when available.** :hourglass: \n"
+            description="### Please upload screenshots of our base requirements and a staff member will help you when available. :hourglass: ###\n"
                         "## **Important:** :loudspeaker: ##\n"
                         "### 1. No Bank Screenshots! :no_entry_sign: :bank: ###\n"
                         "### 2. Full client screenshots with chatbox open :camera: ###\n"
@@ -71,11 +71,11 @@ async def show_rank_panel(thread):
     guild = thread.guild
     embed = discord.Embed(
         title="Rank Up :crossed_swords:",
-        description="# Screenshots within your ticket should contain the following: #\n"
+        description="# Screenshots within your ticket should contain: #\n"
                     "### 1: Full client screenshots with chatbox open :camera: ##\n"
                     "### 2: The requirements in the image for the rank :crossed_swords: ##\n"
                     "### 3: Your in-game name. :bust_in_silhouette: ##\n"
-                    "# Select the button for the rank you want below. #",
+                    "## Select the button for the rank you want below: ##",
         color=discord.Color.green()
     )
 
